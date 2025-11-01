4bcf73ae98dfd7d9044d879fc4397ac5ae22fdd1
guillesd <gsdionis@gmail.com>
2025-11-01 14:38:18 +0100

    try to add tpch_gen function calling tpchgen-rs generators

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 extern crate duckdb;
 extern crate duckdb_loadable_macros;
 extern crate libduckdb_sys;
+extern crate tpchgen_arrow;
+extern crate tpchgen;
 
 use duckdb::{
     core::{DataChunkHandle, Inserter, LogicalTypeHandle, LogicalTypeId},
@@ -9,6 +11,8 @@ use duckdb::{
 };
 use duckdb_loadable_macros::duckdb_entrypoint_c_api;
 use libduckdb_sys as ffi;
+use tpchgen_arrow::{LineItemArrow, OrderArrow, CustomerArrow};
+use tpchgen::generators::{LineItemGenerator, OrderGenerator, CustomerGenerator};
 use std::{
     error::Error,
     ffi::CString,
@@ -62,11 +66,63 @@ impl VTab for HelloVTab {
     }
 }
 
-const EXTENSION_NAME: &str = env!("CARGO_PKG_NAME");
+#[repr(C)]
+struct TpchBindData {
+    sf: f64,
+}
+
+#[repr(C)]
+struct TpchInitData {
+    done: AtomicBool,
+}
+struct TpchGenVTab;
+
+impl VTab for TpchGenVTab {
+    type BindData = TpchBindData;
+    type InitData = TpchInitData;
+
+    fn bind(bind: &BindInfo) -> Result<Self::BindData, Box<dyn std::error::Error>> {
+        bind.add_result_column("status", LogicalTypeHandle::from(LogicalTypeId::Varchar));
+        let sf: f64 = bind.get_parameter(0).to_string().parse().unwrap();
+        Ok(TpchBindData { sf })
+    }
+
+    fn init(_: &InitInfo) -> Result<Self::InitData, Box<dyn std::error::Error>> {
+        Ok(TpchInitData {
+            done: AtomicBool::new(false),
+        })
+    }
+
+    fn func(func: &TableFunctionInfo<Self>, output: &mut DataChunkHandle) -> Result<(), Box<dyn std::error::Error>> {
+        let init_data = func.get_init_data();
+        let bind_data = func.get_bind_data();
+        let line_item  = LineItemGenerator::new(bind_data.sf, 1, 1);
+        let order = OrderGenerator::new(bind_data.sf, 1, 1);
+        let customer  = CustomerGenerator::new(bind_data.sf, 1, 1);
+
+        let mut customer_arrow_generator = CustomerArrow::new(customer);
+        let mut order_arrow_generator = OrderArrow::new(order);
+        let mut line_item_arrow_generator = LineItemArrow::new(line_item);
+
+        //result
+        let vector = output.flat_vector(0);
+        let result = CString::new(format!("Successfully generated TPC-H data"))?;
+        vector.insert(0, result);
+        output.set_len(1);
+        Ok(())
+    }
+
+    fn parameters() -> Option<Vec<LogicalTypeHandle>> {
+        Some(vec![LogicalTypeHandle::from(LogicalTypeId::Varchar)])
+    }
+}
 
 #[duckdb_entrypoint_c_api()]
 pub unsafe fn extension_entrypoint(con: Connection) -> Result<(), Box<dyn Error>> {
-    con.register_table_function::<HelloVTab>(EXTENSION_NAME)
+    con.register_table_function::<HelloVTab>("example_hello_name")
         .expect("Failed to register hello table function");
+
+    con.register_table_function::<TpchGenVTab>("tpch_gen")
+        .expect("Failed to register tpch gen table function");
     Ok(())
 }
