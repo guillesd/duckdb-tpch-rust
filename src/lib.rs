fe6e5ac51b4f6fc74410c571ce5e98811f20781a
guillesd <gsdionis@gmail.com>
2025-12-19 14:18:04 +0100

    use the tpchgen runner exposed in the cli crate library

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
 extern crate duckdb;
 extern crate duckdb_loadable_macros;
 extern crate libduckdb_sys;
-extern crate tpchgen_arrow;
-extern crate tpchgen;
-extern crate arrow;
-extern crate parquet;
+extern crate tpchgen_cli;
+extern crate tokio;
 
 use duckdb::{
     core::{DataChunkHandle, Inserter, LogicalTypeHandle, LogicalTypeId},
@@ -13,16 +11,15 @@ use duckdb::{
 };
 use duckdb_loadable_macros::duckdb_entrypoint_c_api;
 use libduckdb_sys as ffi;
-use tpchgen_arrow::{LineItemArrow, OrderArrow, CustomerArrow};
-use tpchgen::generators::{LineItemGenerator, OrderGenerator, CustomerGenerator};
+use tpchgen_cli::{TpchGenerator, Table, OutputFormat};
+use std::path::PathBuf;
 use std::{
     error::Error,
     ffi::CString,
     sync::atomic::{AtomicBool, Ordering},
 };
-use arrow::record_batch::RecordBatch;
-use parquet::arrow::ArrowWriter;
-use std::fs::File;
+
+
 
 macro_rules! debug_print {
     ($($arg:tt)*) => {
@@ -30,53 +27,6 @@ macro_rules! debug_print {
             eprintln!("[PCAP Debug] {}", format!($($arg)*));
         }
     };
-}
-
-#[repr(C)]
-struct HelloBindData {
-    name: String,
-}
-
-#[repr(C)]
-struct HelloInitData {
-    done: AtomicBool,
-}
-
-struct HelloVTab;
-
-impl VTab for HelloVTab {
-    type InitData = HelloInitData;
-    type BindData = HelloBindData;
-
-    fn bind(bind: &BindInfo) -> Result<Self::BindData, Box<dyn std::error::Error>> {
-        bind.add_result_column("column0", LogicalTypeHandle::from(LogicalTypeId::Varchar));
-        let name = bind.get_parameter(0).to_string();
-        Ok(HelloBindData { name })
-    }
-
-    fn init(_: &InitInfo) -> Result<Self::InitData, Box<dyn std::error::Error>> {
-        Ok(HelloInitData {
-            done: AtomicBool::new(false),
-        })
-    }
-
-    fn func(func: &TableFunctionInfo<Self>, output: &mut DataChunkHandle) -> Result<(), Box<dyn std::error::Error>> {
-        let init_data = func.get_init_data();
-        let bind_data = func.get_bind_data();
-        if init_data.done.swap(true, Ordering::Relaxed) {
-            output.set_len(0);
-        } else {
-            let vector = output.flat_vector(0);
-            let result = CString::new(format!("Rusty Quack {} 🐥", bind_data.name))?;
-            vector.insert(0, result);
-            output.set_len(1);
-        }
-        Ok(())
-    }
-
-    fn parameters() -> Option<Vec<LogicalTypeHandle>> {
-        Some(vec![LogicalTypeHandle::from(LogicalTypeId::Varchar)])
-    }
 }
 
 #[repr(C)]
@@ -107,38 +57,27 @@ impl VTab for TpchGenVTab {
     }
 
     fn func(func: &TableFunctionInfo<Self>, output: &mut DataChunkHandle) -> Result<(), Box<dyn std::error::Error>> {
-        let init_data = func.get_init_data();
         let bind_data = func.get_bind_data();
-        let line_item  = LineItemGenerator::new(bind_data.sf, 1, 1);
-        let order = OrderGenerator::new(bind_data.sf, 1, 1);
-        let customer  = CustomerGenerator::new(bind_data.sf, 1, 1);
-
-        let mut customer_arrow_generator = CustomerArrow::new(customer);
-        let mut order_arrow_generator = OrderArrow::new(order);
-        let mut line_item_arrow_generator = LineItemArrow::new(line_item);
-
-        debug_print!("Writing data...");
-
-        Self::write_batches_to_parquet(
-            customer_arrow_generator,
-            "customer_streaming.parquet"
-        )?;
-
-        Self::write_batches_to_parquet(
-            order_arrow_generator,
-            "order_streaming.parquet"
-        )?;
-
-        Self::write_batches_to_parquet(
-            line_item_arrow_generator,
-            "lineitem_streaming.parquet"
-        )?;
+        let init_data = func.get_init_data();
+        // I need an async runtime to run the generator
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let generator = TpchGenerator::builder()
+            .with_scale_factor(bind_data.sf)
+            .with_output_dir(PathBuf::from("./tmp/data"))
+            .with_tables(vec![Table::Customer, Table::Orders, Table::Lineitem])
+            .with_format(OutputFormat::Parquet)
+            .build();
+        rt.block_on(generator.generate())?;
 
         //result
-        let vector = output.flat_vector(0);
-        let result = CString::new(format!("Successfully generated TPC-H data"))?;
-        vector.insert(0, result);
-        output.set_len(1);
+        if init_data.done.swap(true, Ordering::Relaxed) {
+            output.set_len(0);
+        } else {
+            let vector = output.flat_vector(0);
+            let result = CString::new("Successfully generated TPC-H data")?;
+            vector.insert(0, result);
+            output.set_len(1);
+        }
         Ok(())
     }
 
@@ -147,33 +86,8 @@ impl VTab for TpchGenVTab {
     }
 }
 
-impl TpchGenVTab {
-    // Additional helper methods can be added here if needed
-    fn write_batches_to_parquet(
-    batches: impl Iterator<Item = RecordBatch>,
-    path: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let file = File::create(path)?;
-    let mut batches = batches.peekable();
-    let Some(first_batch) = batches.peek() else {
-        return Ok(()); // no data shrug
-    };
-    let mut writer = ArrowWriter::try_new(file, first_batch.schema(), None)?;
-    
-    for batch in batches {
-        writer.write(&batch)?;
-    }
-    
-    writer.close()?;
-    Ok(())
-}
-}
-
 #[duckdb_entrypoint_c_api()]
 pub unsafe fn extension_entrypoint(con: Connection) -> Result<(), Box<dyn Error>> {
-    con.register_table_function::<HelloVTab>("example_hello_name")
-        .expect("Failed to register hello table function");
-
     con.register_table_function::<TpchGenVTab>("tpch_gen")
         .expect("Failed to register tpch gen table function");
     Ok(())
